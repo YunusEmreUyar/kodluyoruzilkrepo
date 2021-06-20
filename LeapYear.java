0ead9fc840a27ec047bf8aa779650fcaf5d40dc0
Yunus Emre Uyar <yunusemreuyarr@outlook.com>
2021-06-20 14:11:56 +0300

    Practice

---
+++ LeapYear.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        System.out.println("Enter a year: ");
+        int year = in.nextInt();
+
+        if (year%4 == 0 || year%400==0) {
+            System.out.println("This is a leap year.");  
+        } else {
+            System.out.println("It is not a leap year.");
+        }
+
+        in.close();
+    }
+}
