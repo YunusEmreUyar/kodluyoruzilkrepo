ee85d08a25ad9e72719f304c61453e991d141322
Yunus Emre Uyar <yunusemreuyarr@outlook.com>
2021-06-21 15:05:48 +0300

    P13

---
+++ LoopPractice13.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class LoopPractice13 {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        int n = in.nextInt();
+        
+        for(int i=0;i<n;i++){
+
+            for(int j=0;j<=i;j++){
+                System.out.print(" ");
+            }   
+            for(int k=2*n-1;k>=(2*i+1);k--){
+                System.out.print("*");
+            }
+                System.out.println();   
+        }
+        in.close();
+    }
+}
